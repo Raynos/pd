83d0d7971088ace68d6616f9a264b524b90ba5ba
Raynos <raynos2@gmail.com>
2011-10-12 15:46:10 +0100

    Updated pd to be moar awesome

--- src/pd.js
+++ src/pd.js
@@ -14,27 +14,60 @@ var pd = function _pd(obj) {
 // Extend natives. This implements the getOwnPropertyDescriptors as defined in es.next
 pd.extendNatives = function _extendNatives() {
     if (!Object.getOwnPropertyDescriptors) {
-        Object.getOwnPropertyDescriptors = pd;
-    }
-};
-
-// DEPRECATED
-// merges merges all objects passed in. The later objects take preference in clashes
-pd.merge = function _merge() {
-    var o = {};
-    for (var k in arguments) {
-        var obj = arguments[k];
-        Object.keys(obj).forEach(function _each(key) {
-            o[key] = obj[key];
+        Object.defineProperty(Object, "getOwnPropertyDescriptors", {
+            value: pd,
+            configurable: true
         });
     }
-    return o;
+    if (!Object.extend) {
+        Object.defineProperty(Object, "extend", {
+            value: pd.extend,
+            configurable: true
+        });
+    }
+    if (!Object.prototype.new) {
+        Object.defineProperty(Object.prototype, "new", {
+            value: function _new() {
+                var o = Object.create(this);
+                o.constructor && o.constructor.apply(o, arguments);
+                return o;
+            }, 
+            configurable: true
+        });
+    }
+    return pd;
 };
 
-// DEPRECATED
-// object is an Object.create shortcut. Consider it an improved object literal.
-pd.object = function _obj(o) {
-    return Object.create(Object.prototype, pd(o));
+// Mixin does the same as pd.extend except it will
+// overwrite the constructor target with the composition 
+// of the constructor of target and the constructor of source
+pd.mixin = function (target, source) {
+    var constructorTarget = target.constructor,
+        constructorSource = source.constructor;
+        
+    pd.extend(target, source);
+    if (constructorTarget && constructorSource) {
+        target.constructor = function _constructor() {
+            var ret = constructorTarget.apply(this, arguments);
+            constructorSource.apply(this, arguments);
+            return ret;
+        };
+    };
+    return target;
+};
+
+// Extend is an n-ary operator that extends the first argument
+// with the own properties of the other arguments
+// returns the first argument
+pd.extend = function (target) {
+    var objs = Array.prototype.slice.call(arguments, 1);
+    objs.forEach(function (obj) {
+        var props = Object.getOwnPropertyNames(obj);
+        props.forEach(function (key) {
+            target[key] = obj[key];
+        });
+    });
+    return target;
 };
 
 if ("undefined" !== typeof module && module.exports) {
--- test/pd-test.js
+++ test/pd-test.js
@@ -1,102 +1,42 @@
-var pd = require("../src/pd.js"),
-    is = require("vows-is");
-
-var obj = {
-    "foo": "foobar",
-    "baz": /something/,
-    "faz": ["one", "two", "three"],
-    "obj": {
-        "baz": "booz"
-    },
-    get thing() {
-        return 42;    
-    },
-    set thingtwo(v) {
-        this._thingtwo = v;
-    },
-    get thingthree() {
-        return 42;
-    },
-    set thingthree(v) {
-        this._thingthree = v;
-    }
-}
-
-is.partial("property", function _partial(context, name) {
-    return context.context("contains a " + name + " property that")
-        .topic.is.property(name)
-        .vow.it.should.be.ok;
-});
-
-is.partial("haspd", function _partial(context, name) {
-    var o = {};
-    o[name] = Object.getOwnPropertyDescriptor(obj, name);
-
-    return context.
-        vow.it.should.include.object(o);
-});
-
-var one = {
-    "one": "faz",
-    "three": "bar"
-};
-
-var two = {
-    "two": "ni",
-    "three": "baz"
-};
-
-var three = {
-    "three": "bas",
-    "four": "four"
-};
-
-var suite = is.suite("pd").batch()
+var pd = require("../src/pd.js").extendNatives(),
+    assert = require("assert");
     
-    .context("pd")
-        .topic.is(function() { return pd })
-        .vow.it.should.be.a("function")
-        .vow.it.should.have.property("merge")
-        .vow.it.should.have.property("object")
+module.exports = {
+    "test pd exists": function () {
+        assert(pd);
+        assert(pd.mixin);
+        assert(pd.extend);
+    },
+    "test natives exists": function () {
+        assert(Object.extend);
+        assert.deepEqual(Object.extend, pd.extend);
+        assert(Object.prototype.new);
+        assert(Object.getOwnPropertyDescriptors);
+        assert.deepEqual(Object.getOwnPropertyDescriptors, pd);
+    },
+    "test pd": function () {
+    
+    }
+};
 
-        .context("pd({ })")
-            .topic.is(function(pd) {
-                return pd(obj);       
-            })
-            .vow.it.should.be.a("object")
-            .partial("haspd", "foo")
-            .partial("haspd", "baz")
-            .partial("haspd", "faz")
-            .partial("haspd", "obj")
-            .partial("haspd", "thing")
-            .partial("haspd", "thingtwo")
-            .partial("haspd", "thingthree")
-            .parent()
+if (!module.parent) {
+    var tests = Object.keys(module.exports);
+    var errors = []
+    var success = []
+    tests.forEach(function (test) {
+        try {
+            module.exports[test]();
+            success.push(test);
+        } catch (e) {
+            errors.push(e);
+        }
+    });
+    console.log("tests succeeded : ", success.length);
+    console.log("errors : ", errors.length);
+    errors.forEach(function (e) {
+        console.dir(e);
+        console.log(e.stack);
+    });
+};
 
-        .context("pd.merge(one, two, three)")
-            .topic.is(function(pd) {
-                return pd.merge(one, two, three); 
-            })
-            .vow.it.should.include.object({"one": "faz"})
-            .vow.it.should.include.object({"two": "ni"})
-            .vow.it.should.include.object({"three": "bas"})
-            .vow.it.should.include.object({"four": "four"})
-            .parent()
-        
-        .context("pd.object(one)")
-            .topic.is(function(pd) {
-                return pd.object(one);
-            })
-            .vow.it.should.have.property("one", "faz")
-            .vow.it.should.have.property("three", "bar")
-            .parent()
 
-.suite();
-
-if (module.parent) {
-    suite.export(module);
-} else {
-    suite.run({
-        reporter: is.reporter
-    })
-}
