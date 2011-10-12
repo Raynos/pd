107107edd2e196bfee6c9c54db20e802a7189339
Raynos <raynos2@gmail.com>
2011-10-12 15:47:42 +0100

    Not my job to have a minified file

--- src/pd-min.js
+++
@@ -1,1 +0,0 @@
-var pd=function a(a){var b=Object.keys(a),c={};return b.forEach(function d(b){var d=Object.getOwnPropertyDescriptor(a,b);c[b]=d}),c};pd.merge=function b(){var a={};for(var b in arguments){var c=arguments[b];Object.keys(c).forEach(function(b){a[b]=c[b]})}return a},pd.object=function(a){return Object.create(Object.prototype,pd(a))},window.pd=pd
